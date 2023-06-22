05f85d18f2ea633fc4f78262011dfdbe6c1461c7
junek3 <rlarudrms28@gmail.com>
2023-06-22 16:57:27 +0900

    feat: 첨삭요청 request dto 작성
    NotBlank와 Size validation을 적용함.
    Entity로 쉽게 변환이 가능하도록 toEntity 메소드를 작성함.
    toEntity 메소드의 역할을 어떤 클래스에서 담당하도록 할지 좀 더 고민해봐야 함.

---
+++ src/main/java/com/example/sulsul/essay/dto/request/CreateEssayRequest.java
@@ -0,0 +1,47 @@
+package com.example.sulsul.essay.dto.request;
+
+import com.example.sulsul.essay.entity.Essay;
+import com.example.sulsul.essay.entity.type.EssayState;
+import com.example.sulsul.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class CreateEssayRequest {
+    @NotBlank
+    @Size(min = 2, max = 20, message = "대학이름은 2글자 이상 20글자 이하입니다.")
+    private String univ;
+
+    @NotBlank
+    @Size(min = 2, max = 8, message = "시험년도는 2글자 이상 8글자 이하입니다.")
+    private String year;
+
+    @NotBlank
+    @Size(min = 2, max = 8, message = "논술분야는 2글자 이상 8글자 이하입니다.")
+    private String eType;
+
+    @NotBlank
+    @Size(min = 2, max = 200, message = "문의사항은 2글자 이상 200글자 이하입니다.")
+    private String inquiry;
+
+    private MultipartFile file;
+
+    public Essay toEntity(User student, User teacher) {
+        return Essay.builder()
+                .univ(univ)
+                .year(year)
+                .eType(eType)
+                .inquiry(inquiry)
+                .essayState(EssayState.REQUEST)
+                .student(student)
+                .teacher(teacher)
+                .build();
+    }
+}
