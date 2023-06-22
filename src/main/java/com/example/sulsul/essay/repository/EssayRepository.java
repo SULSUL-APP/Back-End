10369e4accc90724b166f908da556f6ecd4dcdd6
junek3 <rlarudrms28@gmail.com>
2023-06-22 21:39:16 +0900

    feat: EssayRepository의 쿼리메소드에 주석 작성

--- src/main/java/com/example/sulsul/essay/repository/EssayRepository.java
+++ src/main/java/com/example/sulsul/essay/repository/EssayRepository.java
@@ -1,29 +1,40 @@
 package com.example.sulsul.essay.repository;
 
-import com.example.sulsul.common.type.EType;
 import com.example.sulsul.essay.entity.Essay;
+import com.example.sulsul.essay.entity.type.EssayState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EssayRepository extends JpaRepository<Essay, Long> {
 
-    // POST /essay
     // POST /profiles/{profileId}/essay - save
 
-    // GET /essay/request - findByETypeAndTeacher / findByETypeAndStudent
+    // GET /essay/request - findAllByTeacherAndEssayState, findAllByStudentAndEssayState
     // GET /essay/proceed
     // GET /essay/reject
     // GET /essay/complete
-    List<Essay> findAllByETypeAndTeacher(EType essayType, Long teacherId);
-    List<Essay> findAllByETypeAndStudent(EType essayType, Long studentId);
+
+    /**
+     * 강사가 자신에게 요청된 첨삭목록을 조회한다.
+     * @param teacherId 강사의 userId
+     * @param essayState 조회할 첨삭상태
+     * @return 지정한 첨삭상태의 첨삭목록을 반환한다.
+     */
+    List<Essay> findAllByTeacherAndEssayState(Long teacherId, EssayState essayState);
+
+    /**
+     * 학생이 자신이 요청한 첨삭목록을 조회한다.
+     * @param studentId 학생의 userId
+     * @param essayState 조회할 첨삭상태
+     * @return 지정한 첨삭상태의 첨삭목록을 반환한다.
+     */
+    List<Essay> findAllByStudentAndEssayState(Long studentId, EssayState essayState);
 
     // GET /essay/request/{essayId} - findEssayById
     // GET /essay/request/{essayId}
     // GET /essay/request/{essayId}
     // GET /essay/request/{essayId}
-    Optional<Essay> findEssayById(Long essayId);
 
     // PUT /essay/{essayId}/accept - save
     // PUT /essay/{essayId}/reject
