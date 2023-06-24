ffe123a478e4190aa1ef1d680f16a1dae7c57c79
junek3 <rlarudrms28@gmail.com>
2023-06-24 20:32:41 +0900

    test: EssayRepository 메소드 테스트
    첨삭개별조회 테스트
    첨삭상태변경 테스트
    강사에게 요청된 첨삭목록 조회 테스트
    학생이 요청한 첨삭목록 조회 테스트

--- src/test/java/com/example/sulsul/essay/repository/EssayRepositoryTest.java
+++ src/test/java/com/example/sulsul/essay/repository/EssayRepositoryTest.java
@@ -13,8 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+
+// TODO: 첨삭파일 부분 점검 필요
 
 @DataJpaTest
 class EssayRepositoryTest {
@@ -50,12 +54,22 @@ class EssayRepositoryTest {
                 .uType(UType.TEACHER)
                 .eType(EType.NATURE)
                 .loginType(LoginType.KAKAO)
+                .catchPhrase("항상 최선을 다하겠습니다. 화이링")
+                .build();
+
+        User t2 = User.builder()
+                .name("전용수")
+                .email("smc@gmail.com")
+                .uType(UType.TEACHER)
+                .eType(EType.SOCIETY)
+                .loginType(LoginType.KAKAO)
                 .catchPhrase("항상 최선을 다하겠습니다.")
                 .build();
 
         userRepository.save(s1);
         userRepository.save(s2);
         userRepository.save(t1);
+        userRepository.save(t2);
     }
 
     @Test
@@ -64,10 +78,14 @@ class EssayRepositoryTest {
         User s1 = userRepository.findByEmail("sulsul@gmail.com").get();
         User s2 = userRepository.findByEmail("sulsul@g.hongik.ac.kr").get();
         User t1 = userRepository.findByEmail("sulsul@naver.com").get();
+        User t2 = userRepository.findByEmail("smc@gmail.com").get();
         // then
-        assertThat(s1.getName()).isEqualTo("김경근");
-        assertThat(s2.getName()).isEqualTo("류동완");
-        assertThat(t1.getName()).isEqualTo("임탁균");
+        assertAll(
+                () -> assertThat(s1.getName()).isEqualTo("김경근"),
+                () -> assertThat(s2.getName()).isEqualTo("류동완"),
+                () -> assertThat(t1.getName()).isEqualTo("임탁균"),
+                () -> assertThat(t2.getName()).isEqualTo("전용수")
+        );
     }
 
     @Test
@@ -94,6 +112,153 @@ class EssayRepositoryTest {
                 () -> assertThat(foundEssay.getExamYear()).isEqualTo("2022"),
                 () -> assertThat(foundEssay.getEType()).isEqualTo("수리"),
                 () -> assertThat(foundEssay.getEssayState()).isEqualTo(EssayState.REQUEST),
+                () -> assertThat(foundEssay.getReviewState()).isEqualTo(ReviewState.OFF),
+                () -> assertThat(foundEssay.getStudent()).isEqualTo(s1),
+                () -> assertThat(foundEssay.getTeacher()).isEqualTo(t1)
+        );
+    }
+
+    @Test
+    void 첨삭개별조회_테스트() {
+        // given
+        User s1 = userRepository.findByEmail("sulsul@gmail.com").get();
+        User t1 = userRepository.findByEmail("sulsul@naver.com").get();
+
+        Essay essay = Essay.builder()
+                .univ("홍익대")
+                .examYear("2022")
+                .eType("수리")
+                .inquiry("2022년 수리논술 3번 문제까지 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s1)
+                .teacher(t1)
+                .build();
+        // when
+        Long essayId = essayRepository.save(essay).getId();
+        Essay foundEssay = essayRepository.findById(essayId).get();
+        // then
+        assertAll(
+                () -> assertThat(foundEssay.getUniv()).isEqualTo("홍익대"),
+                () -> assertThat(foundEssay.getExamYear()).isEqualTo("2022"),
+                () -> assertThat(foundEssay.getEType()).isEqualTo("수리"),
+                () -> assertThat(foundEssay.getEssayState()).isEqualTo(EssayState.REQUEST),
+                () -> assertThat(foundEssay.getReviewState()).isEqualTo(ReviewState.OFF),
+                () -> assertThat(foundEssay.getStudent()).isEqualTo(s1),
+                () -> assertThat(foundEssay.getTeacher()).isEqualTo(t1)
+        );
+    }
+
+    @Test
+    void 강사에게_요청된_첨삭목록_조회_테스트() {
+        // given
+        User s1 = userRepository.findByEmail("sulsul@gmail.com").get();
+        User s2 = userRepository.findByEmail("sulsul@g.hongik.ac.kr").get();
+        User t1 = userRepository.findByEmail("sulsul@naver.com").get();
+
+        Essay essay1 = Essay.builder()
+                .univ("홍익대")
+                .examYear("2022")
+                .eType("수리")
+                .inquiry("2022년 수리논술 3번 문제까지 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s1)
+                .teacher(t1)
+                .build();
+
+        Essay essay2 = Essay.builder()
+                .univ("홍익대")
+                .examYear("2023")
+                .eType("인문사회")
+                .inquiry("2023년 인문 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s2)
+                .teacher(t1)
+                .build();
+
+        essayRepository.save(essay1);
+        essayRepository.save(essay2);
+        // when
+        List<Essay> essays = essayRepository.findAllByTeacherIdAndEssayState(t1.getId(), EssayState.REQUEST);
+        // then
+        assertAll(
+                () -> assertThat(essays.size()).isEqualTo(2),
+                () -> assertThat(essays.get(0).getStudent()).isEqualTo(s1),
+                () -> assertThat(essays.get(1).getStudent()).isEqualTo(s2)
+        );
+    }
+
+    @Test
+    void 학생이_요청한_첨삭목록_조회_테스트() {
+        // given
+        User s1 = userRepository.findByEmail("sulsul@gmail.com").get();
+        User t1 = userRepository.findByEmail("sulsul@naver.com").get();
+        User t2 = userRepository.findByEmail("smc@gmail.com").get();
+
+        Essay essay1 = Essay.builder()
+                .univ("홍익대")
+                .examYear("2022")
+                .eType("수리")
+                .inquiry("2022년 수리논술 3번 문제까지 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s1)
+                .teacher(t1)
+                .build();
+
+        Essay essay2 = Essay.builder()
+                .univ("홍익대")
+                .examYear("2023")
+                .eType("인문사회")
+                .inquiry("2023년 인문 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s1)
+                .teacher(t2)
+                .build();
+
+        essayRepository.save(essay1);
+        essayRepository.save(essay2);
+        // when
+        List<Essay> essays = essayRepository.findAllByStudentIdAndEssayState(s1.getId(), EssayState.REQUEST);
+        // then
+        assertAll(
+                () -> assertThat(essays.size()).isEqualTo(2),
+                () -> assertThat(essays.get(0).getTeacher()).isEqualTo(t1),
+                () -> assertThat(essays.get(1).getTeacher()).isEqualTo(t2)
+        );
+    }
+
+    @Test
+    void 첨삭상태_변경_테스트() {
+        // given
+        User s1 = userRepository.findByEmail("sulsul@gmail.com").get();
+        User t1 = userRepository.findByEmail("sulsul@naver.com").get();
+
+        Essay essay = Essay.builder()
+                .univ("홍익대")
+                .examYear("2022")
+                .eType("수리")
+                .inquiry("2022년 수리논술 3번 문제까지 첨삭 부탁드립니다.")
+                .essayState(EssayState.REQUEST)
+                .reviewState(ReviewState.OFF)
+                .student(s1)
+                .teacher(t1)
+                .build();
+        // when
+        Essay acceptedEssay = essayRepository.save(essay);
+        acceptedEssay.updateEssayState(EssayState.PROCEED);
+        essayRepository.save(acceptedEssay);
+
+        Essay foundEssay = essayRepository.findById(acceptedEssay.getId()).get();
+        // then
+        assertAll(
+                () -> assertThat(foundEssay.getUniv()).isEqualTo("홍익대"),
+                () -> assertThat(foundEssay.getExamYear()).isEqualTo("2022"),
+                () -> assertThat(foundEssay.getEType()).isEqualTo("수리"),
+                () -> assertThat(foundEssay.getEssayState()).isEqualTo(EssayState.PROCEED),
                 () -> assertThat(foundEssay.getReviewState()).isEqualTo(ReviewState.OFF),
                 () -> assertThat(foundEssay.getStudent()).isEqualTo(s1),
                 () -> assertThat(foundEssay.getTeacher()).isEqualTo(t1)
