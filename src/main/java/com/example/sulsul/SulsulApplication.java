598a1f3f03a8d55ff557698439a8a1e9652517da
junek3 <rlarudrms28@gmail.com>
2023-06-27 01:01:32 +0900

    refactor: EnableJpaAuditing 분리
    Controller 슬라이스 테스트를 위해
    EnableJpaAuditing을 설정 파일로 분리함

--- src/main/java/com/example/sulsul/SulsulApplication.java
+++ src/main/java/com/example/sulsul/SulsulApplication.java
@@ -2,7 +2,6 @@ package com.example.sulsul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class SulsulApplication {
