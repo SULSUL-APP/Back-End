b5ec20e3033ef8c8447475683401f236114535cf
junek3 <rlarudrms28@gmail.com>
2023-06-29 00:11:01 +0900

    feat: ddl-auto create 과정에서 데모데이터 생성하기

--- src/test/java/com/example/sulsul/essay/repository/EssayRepositoryTest.java
+++ src/test/java/com/example/sulsul/essay/repository/EssayRepositoryTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class EssayRepositoryTest {
 
     @Autowired
