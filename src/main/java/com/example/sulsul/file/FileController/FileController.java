a3954132081c1d2cd55bd47cb1f3f2ff0e3cafd9
junek3 <rlarudrms28@gmail.com>
2023-06-28 17:43:07 +0900

    refactor: FileService 리팩토링
    엔티티 생성 로직과 S3 버킷 업로드 로직을 분리함
    불필요한 import문 제거

--- src/main/java/com/example/sulsul/file/FileController/FileController.java
+++ src/main/java/com/example/sulsul/file/FileController/FileController.java
@@ -1,14 +1,7 @@
 package com.example.sulsul.file.FileController;
 
-import com.example.sulsul.common.type.EType;
-import com.example.sulsul.common.type.LoginType;
-import com.example.sulsul.common.type.UType;
-import com.example.sulsul.essay.entity.Essay;
-import com.example.sulsul.essay.entity.type.EssayState;
-import com.example.sulsul.essay.entity.type.ReviewState;
 import com.example.sulsul.file.dto.uploadFileRequest;
 import com.example.sulsul.file.service.FileService;
-import com.example.sulsul.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
