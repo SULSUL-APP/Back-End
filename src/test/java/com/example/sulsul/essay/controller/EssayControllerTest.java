14ce1a0d0c9be8244289067184e8d6e13390d6f3
junek3 <rlarudrms28@gmail.com>
2023-06-29 17:15:09 +0900

    test: EssayController 테스트코드 작성
    GET /essay/request
    첨삭요청 목록 조회 api 테스트

--- src/test/java/com/example/sulsul/essay/controller/EssayControllerTest.java
+++ src/test/java/com/example/sulsul/essay/controller/EssayControllerTest.java
@@ -1,14 +1,14 @@
 package com.example.sulsul.essay.controller;
 
 import com.example.sulsul.comment.entity.Comment;
+import com.example.sulsul.common.type.EssayState;
+import com.example.sulsul.common.type.ReviewState;
 import com.example.sulsul.essay.DemoDataFactory;
 import com.example.sulsul.essay.dto.request.CreateEssayRequest;
 import com.example.sulsul.essay.dto.response.ProceedEssayResponse;
+import com.example.sulsul.essay.dto.response.RequestEssayResponse;
 import com.example.sulsul.essay.entity.Essay;
-import com.example.sulsul.common.type.EssayState;
-import com.example.sulsul.common.type.ReviewState;
 import com.example.sulsul.essay.service.EssayService;
-import com.example.sulsul.file.entity.File;
 import com.example.sulsul.file.service.FileService;
 import com.example.sulsul.user.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,17 +19,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -67,9 +65,13 @@ class EssayControllerTest {
         String contentType = "pdf";
         String filePath = "src/test/resources/pdf/test.pdf";
         MockMultipartFile testFile = getMockMultipartFile(fileName, contentType, filePath);
+        // 학생이 업로드한 첨삭파일의 파일경로
+        String studentFilePath = "https://sulsul.s3.ap-northeast-2.amazonaws.com/files/314a32f7_sulsul.pdf";
         // stub
         when(essayService.createEssay(eq(1L), any(User.class), any(CreateEssayRequest.class)))
                 .thenReturn(essay1);
+        when(essayService.getEssayResponseWithStudentFile(eq(1L)))
+                .thenReturn(new RequestEssayResponse(essay1, studentFilePath));
         // when && then
         mockMvc.perform(multipart("/profiles/{profileId}/essay", 1L)
                         .file("essayFile", testFile.getBytes())
@@ -79,12 +81,13 @@ class EssayControllerTest {
                         .param("eType", "수리"))
                 .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.essayId").value(1L))
+                .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.teacher.name").value("임탁균"))
                 .andExpect(jsonPath("$.teacher.email").value("sulsul@naver.com"))
                 .andExpect(jsonPath("$.teacher.catchPhrase").value("항상 최선을 다하겠습니다. 화이링"))
                 .andExpect(jsonPath("$.student.name").value("김경근"))
-                .andExpect(jsonPath("$.student.email").value("sulsul@gmail.com"));
+                .andExpect(jsonPath("$.student.email").value("sulsul@gmail.com"))
+                .andExpect(jsonPath("$.studentFilePath").value(studentFilePath));
     }
 
     @Test
@@ -113,17 +116,52 @@ class EssayControllerTest {
         mockMvc.perform(multipart("/essay/proceed/{essayId}/upload", 1L)
                         .file("essayFile", testFile.getBytes()))
                 .andDo(print())
-                .andExpect(status().isCreated());
-//                .andExpect(jsonPath("$.essayId").value(1L))
-//                .andExpect(jsonPath("$.teacher.name").value("임탁균"))
-//                .andExpect(jsonPath("$.teacher.email").value("sulsul@naver.com"))
-//                .andExpect(jsonPath("$.teacher.catchPhrase").value("항상 최선을 다하겠습니다. 화이링"))
-//                .andExpect(jsonPath("$.student.name").value("김경근"))
-//                .andExpect(jsonPath("$.student.email").value("sulsul@gmail.com"));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.teacher.name").value("임탁균"))
+                .andExpect(jsonPath("$.teacher.email").value("sulsul@naver.com"))
+                .andExpect(jsonPath("$.teacher.catchPhrase").value("항상 최선을 다하겠습니다. 화이링"))
+                .andExpect(jsonPath("$.student.name").value("김경근"))
+                .andExpect(jsonPath("$.student.email").value("sulsul@gmail.com"))
+                .andExpect(jsonPath("$.studentFilePath").value(studentFilePath))
+                .andExpect(jsonPath("$.teacherFilePath").value(teacherFilePath))
+                .andExpect(jsonPath("$.comments[0].commentId").value(1L))
+                .andExpect(jsonPath("$.comments[0].user.name").value("김경근"))
+                .andExpect(jsonPath("$.comments[1].commentId").value(2L))
+                .andExpect(jsonPath("$.comments[1].user.name").value("임탁균"));
     }
 
     @Test
-    void getRequestEssays() {
+    @DisplayName("첨삭요청 목록 조회 GET /essay/request")
+    void getRequestEssaysTest() throws Exception {
+        // given
+        User s1 = DemoDataFactory.createStudent1(1L);
+        User t1 = DemoDataFactory.createTeacher1(2L);
+        User t2 = DemoDataFactory.createTeacher2(3L);
+        Essay essay1 = DemoDataFactory.createEssay1(1L, s1, t1, EssayState.REQUEST, ReviewState.OFF);
+        Essay essay2 = DemoDataFactory.createEssay2(2L, s1, t2, EssayState.REQUEST, ReviewState.OFF);
+        // stub
+        when(essayService.getEssaysByUser(any(User.class), eq(EssayState.REQUEST))).thenReturn(List.of(essay1, essay2));
+        // when && then
+        mockMvc.perform(get("/essay/request"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.essays[0].id").value(1L))
+                .andExpect(jsonPath("$.essays[0].univ").value("홍익대"))
+                .andExpect(jsonPath("$.essays[0].examYear").value("2022"))
+                .andExpect(jsonPath("$.essays[0].teacher.name").value("임탁균"))
+                .andExpect(jsonPath("$.essays[0].teacher.email").value("sulsul@naver.com"))
+                .andExpect(jsonPath("$.essays[0].teacher.catchPhrase").value("항상 최선을 다하겠습니다. 화이링"))
+                .andExpect(jsonPath("$.essays[0].student.name").value("김경근"))
+                .andExpect(jsonPath("$.essays[0].student.email").value("sulsul@gmail.com"))
+                .andExpect(jsonPath("$.essays[1].id").value(2L))
+                .andExpect(jsonPath("$.essays[1].univ").value("홍익대"))
+                .andExpect(jsonPath("$.essays[1].examYear").value("2023"))
+                .andExpect(jsonPath("$.essays[1].teacher.name").value("전용수"))
+                .andExpect(jsonPath("$.essays[1].teacher.email").value("smc@gmail.com"))
+                .andExpect(jsonPath("$.essays[1].teacher.catchPhrase").value("항상 최선을 다하겠습니다."))
+                .andExpect(jsonPath("$.essays[1].student.name").value("김경근"))
+                .andExpect(jsonPath("$.essays[1].student.email").value("sulsul@gmail.com"));
     }
 
     @Test
