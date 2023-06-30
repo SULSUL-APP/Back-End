29a4fdc1bda688fd825ebc349755214398d5b1af
junek3 <rlarudrms28@gmail.com>
2023-06-30 23:57:23 +0900

    test: EssayController 테스트코드 작성
    GET /essay/proceed
    진행중인 첨삭목록 조회 api 테스트

--- src/main/java/com/example/sulsul/common/type/EssayState.java
+++ src/main/java/com/example/sulsul/common/type/EssayState.java
@@ -1,21 +1,8 @@
 package com.example.sulsul.common.type;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum EssayState {
-    REQUEST("첨삭요청"),
-    PROCEED("첨삭진행"),
-    REJECT("첨삭거절"),
-    COMPLETE("첨삭완료");
-
-    private final String state;
-
-    EssayState(String state) {
-        this.state = state;
-    }
-
-    @JsonValue // Json으로 변환할 때 Enum의 값을 어떻게 표현할지 결정
-    public String getState() {
-        return this.state;
-    }
+    REQUEST,
+    PROCEED,
+    REJECT,
+    COMPLETE;
 }
