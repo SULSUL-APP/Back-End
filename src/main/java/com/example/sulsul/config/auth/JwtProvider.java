540a8f2a20750c29b322a885046cefe3dcc93cdd
ImTakGyun <gyun1712@gmail.com>
2023-07-10 00:15:32 +0900

    feat: DataBase 설정 추가

--- src/main/java/com/example/sulsul/config/auth/JwtProvider.java
+++ src/main/java/com/example/sulsul/config/auth/JwtProvider.java
@@ -3,6 +3,7 @@ package com.example.sulsul.config.auth;
 import com.example.sulsul.user.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
