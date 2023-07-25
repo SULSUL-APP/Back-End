3653fedf4b986f486a4050d9ea1cc05cee089d24
junek3 <rlarudrms28@gmail.com>
2023-07-25 22:49:43 +0900

    feat: ErrorResponse에 Schema 어노테이션 작성

--- src/main/java/com/example/sulsul/exceptionhandler/ErrorResponse.java
+++ src/main/java/com/example/sulsul/exceptionhandler/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.example.sulsul.exceptionhandler;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +8,13 @@ import java.util.Map;
 
 @Getter
 public class ErrorResponse {
-
+    @Schema(description = "에러 코드", example = "COMMENT_02")
     private final String code;
+
+    @Schema(description = "에러 메세지", example = "댓글 생성에 실패했습니다.")
     private final String messages;
+
+    @Schema(description = "에러 세부내용", example = "{\"detail\":\"댓글은 2글자 이상 100글자 이하입니다.\"}")
     private final Map<String, String> errors;
 
     @Builder
