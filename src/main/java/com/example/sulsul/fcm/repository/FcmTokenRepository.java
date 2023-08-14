7057fa3c869776b39cf037f94f25c64e16a1a6ad
junek3 <rlarudrms28@gmail.com>
2023-08-14 21:15:52 +0900

    feat: FcmTokenRepository에 주석추가

--- src/main/java/com/example/sulsul/fcm/repository/FcmTokenRepository.java
+++ src/main/java/com/example/sulsul/fcm/repository/FcmTokenRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+    /**
+     * 유저의 FcmToken 조회
+     *
+     * @param user FcmToken을 조회할 유저
+     * @return FcmToken 반환
+     */
     Optional<FcmToken> findByUser(User user);
-
-
-
 }
