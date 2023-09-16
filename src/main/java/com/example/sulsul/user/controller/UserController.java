e409394cee0039ab40bf788b6347b13aa9a9a8d4
TakGyun <gyun1712@gmail.com>
2023-09-16 20:57:57 +0900

    feat: 마이페이지 조회 구현

--- src/main/java/com/example/sulsul/user/controller/UserController.java
+++ src/main/java/com/example/sulsul/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.sulsul.user.controller;
 
 import com.example.sulsul.common.CurrentUser;
 import com.example.sulsul.exceptionhandler.ErrorResponse;
+import com.example.sulsul.review.dto.response.ReviewGroupResponse;
 import com.example.sulsul.user.dto.response.LoginResponse;
 import com.example.sulsul.user.dto.request.SignUpRequest;
 import com.example.sulsul.user.entity.User;
@@ -14,6 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,13 +39,34 @@ public class UserController {
                     content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping(value = "/sign-up")
-    public ResponseEntity<Void> signUp(@CurrentUser User user,
-                                       @RequestBody SignUpRequest signUpRequest) {
+    public ResponseEntity<?> signUp(@CurrentUser User user,
+                                    @RequestBody SignUpRequest signUpRequest) {
 
         log.info("[현재 로그인한 유저]: {}", user.getEmail());
         userService.signUp(user, signUpRequest);
 
-        return ResponseEntity.noContent().build();
+        return new ResponseEntity<>(new LoginResponse(user), HttpStatus.OK);
     }
 
+    @Operation(summary = "마이페이지 조희", description = "유저의 마이페이지를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "FORBIDDEN",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @GetMapping(value = "/mypage")
+    public ResponseEntity<?> getMyPage(@CurrentUser User user) {
+
+        log.info("[현재 로그인한 유저]: {}", user.getEmail());
+
+        if(user.isTeacher())
+            return new ResponseEntity<>(userService.getTeacherMyPage(user), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(userService.getStudentMyPage(user), HttpStatus.OK);
+    }
 }
